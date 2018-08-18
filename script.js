e9f9c2067ad5ac7371c0d9b6e7c800602996acdd
FerdinandNathaniel <fabian.fn.kok@gmail.com>
2018-08-18 20:00:26 +0200

    last commit of day, lots of TO-DO's

--- script.js
+++ script.js
@@ -13,3 +13,40 @@
     ftypes[4]='phone';
 }
 (jQuery));
+
+// Email Set-up, making fields required
+// TO-DO: make this work, doesn't atm
+document.getElementById("mce-EMAIL").required = true;
+document.getElementById('mce-FNAME').required = true;
+document.getElementById('mce-LNAME').required = true;
+
+// Creating various scroll speeds
+$.fn.moveIt = function(){
+    var $window = $(window);
+    var instances = [];
+    
+    $(this).each(function(){
+      instances.push(new moveItItem($(this)));
+    });
+    
+    window.addEventListener('scroll', function(){
+      var scrollTop = $window.scrollTop();
+      instances.forEach(function(inst){
+        inst.update(scrollTop);
+      });
+    }, {passive: true});
+  }
+  
+  var moveItItem = function(el){
+    this.el = $(el);
+    this.speed = parseInt(this.el.attr('data-scroll-speed'));
+  };
+  
+  moveItItem.prototype.update = function(scrollTop){
+    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+  };
+  
+  // Initialization
+  $(function(){
+    $('[data-scroll-speed]').moveIt();
+  });
