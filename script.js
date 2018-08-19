da38d4cb67deca683c3bb8f2df0cd5381a1a1b0d
FerdinandNathaniel <fabian.fn.kok@gmail.com>
2018-08-19 09:56:49 +0200

    added background image

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-(function($) {
-    window.fnames = new Array(); 
-    window.ftypes = new Array();
-    fnames[0]='EMAIL';
-    ftypes[0]='email';
-    fnames[1]='FNAME';
-    ftypes[1]='text';
-    fnames[2]='LNAME';
-    ftypes[2]='text';
-    fnames[3]='ADDRESS';
-    ftypes[3]='address';
-    fnames[4]='PHONE';
-    ftypes[4]='phone';
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[1] = 'FNAME';
+  ftypes[1] = 'text';
+  fnames[2] = 'LNAME';
+  ftypes[2] = 'text';
+  fnames[3] = 'ADDRESS';
+  ftypes[3] = 'address';
+  fnames[4] = 'PHONE';
+  ftypes[4] = 'phone';
 }
-(jQuery));
+  (jQuery));
 
 // Email Set-up, making fields required
 // TO-DO: make this work, doesn't atm
@@ -21,32 +21,32 @@ document.getElementById('mce-FNAME').required = true;
 document.getElementById('mce-LNAME').required = true;
 
 // Creating various scroll speeds
-$.fn.moveIt = function(){
-    var $window = $(window);
-    var instances = [];
-    
-    $(this).each(function(){
-      instances.push(new moveItItem($(this)));
-    });
-    
-    window.addEventListener('scroll', function(){
-      var scrollTop = $window.scrollTop();
-      instances.forEach(function(inst){
-        inst.update(scrollTop);
-      });
-    }, {passive: true});
-  }
-  
-  var moveItItem = function(el){
-    this.el = $(el);
-    this.speed = parseInt(this.el.attr('data-scroll-speed'));
-  };
-  
-  moveItItem.prototype.update = function(scrollTop){
-    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-  };
-  
-  // Initialization
-  $(function(){
-    $('[data-scroll-speed]').moveIt();
+$.fn.moveIt = function () {
+  var $window = $(window);
+  var instances = [];
+
+  $(this).each(function () {
+    instances.push(new moveItItem($(this)));
   });
+
+  window.addEventListener('scroll', function () {
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function (inst) {
+      inst.update(scrollTop);
+    });
+  }, { passive: true });
+}
+
+var moveItItem = function (el) {
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function (scrollTop) {
+  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+};
+
+// Initialization
+$(function () {
+  $('[data-scroll-speed]').moveIt();
+});
